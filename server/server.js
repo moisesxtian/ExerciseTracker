50d826b61b7e258e77775c4d7473ee1a1371285f
Hyx <christiansmoises023@gmail.com>
2025-05-24 00:37:15 +0800

    Updated the Overall functionality
    Added Mongoose Connection
    Deleted Default '/' Route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
-//server.js
 const express = require('express');
 const app = express();
-const port = 3000;
+const workoutRoutes = require('./routes/workouts');
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+//dotenv
+const dotenv = require('dotenv');
+dotenv.config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+const mongoose = require('mongoose');
+const dbURI = process.env.MONGODB_URI;
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+// Middleware to handle CORS
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/workouts', workoutRoutes);
+// Connect to MongoDB and start the server
+mongoose.connect(dbURI)
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Example app listening at http://localhost:${port}`);
+        });
+                console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
+
+
